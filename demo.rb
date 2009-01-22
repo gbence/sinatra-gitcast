c4228494e84c6fd950160ee99da29e852d8bf1cc
Golda Bence <gbence@algernon.hu>
2009-01-22 16:17:22 +0100

    Routing and template variables.

--- demo.rb
+++ demo.rb
@@ -4,5 +4,9 @@ require 'rubygems'
 require 'sinatra'
 
 get '/' do
-  haml :index
+  haml :index, :locals => { :name => 'world' }
+end
+
+get '/:name' do
+  haml :index, :locals => { :name => params[:name] }
 end
