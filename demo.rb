72acb74826f83d759d3408fb5085b24413da816c
Golda Bence <gbence@algernon.hu>
2009-01-22 16:24:41 +0100

    Some CSS with SASS.

--- demo.rb
+++ demo.rb
@@ -7,6 +7,11 @@ get '/' do
   haml :index, :locals => { :name => 'world' }
 end
 
+get '/demo.css' do
+  content_type 'text/css', :charset => 'utf-8'
+  sass :demo
+end
+
 get '/:name' do
   haml :index, :locals => { :name => params[:name] }
 end
